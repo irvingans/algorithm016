9986cf8ef59fd8037912a6ee0e584117d0a3a067
irvingtheans <edmondkopchen@gmail.com>
2020-10-04 20:44:35 +0800

    week6 homework min path sum

---
+++ Week_06/minpath.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public int minPathSum(int[][] grid) {
+        int n= grid.length;
+        if(n==0){
+            return 0;
+        }
+        int m = grid[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0&&j>0){
+                    grid[i][j]+=grid[i][j-1];
+                }
+                else if(i>0&&j==0){
+                    grid[i][j]+=grid[i-1][j];
+                }
+                else if(i>0&&j>0){
+                    grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
+                }
+            }
+        }
+        return grid[n-1][m-1]; 
+    }
+}
